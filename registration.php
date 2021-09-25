ed839c6ccf63b301fc0dd361ee7c85e482fa2d96
Wishusucess <90965189+Wishusucess@users.noreply.github.com>
2021-09-25 15:14:48 +0530

    Add files via upload

---
+++ registration.php
@@ -0,0 +1,12 @@
+<?php
+/*
+ * @Author      Hemant Singh
+ * @Developer   Hemant Singh
+ * @Module      Wishusucess_ShippingTextarea
+ * @copyright   Copyright (c) Wishusucess (http://www.wishusucess.com/)
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+\Magento\Framework\Component\ComponentRegistrar::MODULE,
+'Wishusucess_ShippingTexarea',
+__DIR__
+);
